202ba2627796a0cd50590e809936076b641cfbb8
mwithero <33492855+mwithero@users.noreply.github.com>
2021-02-25 20:41:48 -0500

    ignore

---
+++ SpotifyAPI/secrets.js
@@ -0,0 +1,20 @@
+//using *https://www.npmjs.com/package/spotify-web-api-node* package for spotify stuff
+
+//clientId: 'b9d72821ddbe43bbb0a8242c7870c117'
+//clientSecret: '301a476e613043d2b9e1753528a746ba'
+//GET https://accounts.spotify.com/authorize
+//client_id
+//response_type
+//using expo for ios testing made easy 
+
+export const spotifyCredentials = {
+	clientId: 'b9d72821ddbe43bbb0a8242c7870c117'
+	clientSecret: '301a476e613043d2b9e1753528a746ba'
+	redirectUri: 'https://expo.io/accounts/mwithero/projects/SpotifyAPI_request_test_expo'
+}
+
+
+
+//var SpotifyWebApi = require('spotify-web-api-node');
+
+//var spotifyApi = new SpotifyWebApi({ clientId: 'b9d72821ddbe43bbb0a8242c7870c117', clientSecret: '301a476e613043d2b9e1753528a746ba', redirectUri: '' });
