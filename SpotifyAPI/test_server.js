7abbcf422fa94278f157444c84ceff05fb477cd4
mwithero <33492855+mwithero@users.noreply.github.com>
2021-02-25 19:22:39 -0500

    Downloaded expo for spotify integration with React native

--- SpotifyAPI/test_server.js
+++ SpotifyAPI/test_server.js
@@ -1,4 +1,7 @@
 //Simple http server to use for redirectUri field in auth_check.js
+//go to SpotifyAPI directory and run node test_server.js
+// the url is localhost:6969 and prints "Hello World" 
+
 
 var http = require("http");
 
