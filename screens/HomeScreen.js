d21b945a79677fd7a12c8fd4e21ac70d7ca32e67
KyleKMZ <zaw@purdue.edu>
2021-02-27 12:06:13 +0630

    Landing page section titles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,42 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { Text, View, FlatList, StyleSheet } from 'react-native'
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <Text> HOME SCREEN </Text>
+    <View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.title}>Curated Playlists</Text>
+        <View style={styles.horizontalRule}></View>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.title}>Live Performances</Text>
+        <View style={styles.horizontalRule}></View>
+      </View>
+      <View style={styles.sectionContainer}>
+        <Text style={styles.title}>Statistics</Text>
+        <View style={styles.horizontalRule}></View>
+      </View>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  horizontalRule: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1.0,
+    width: 250,
+  },
+  sectionContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#8e6f3e',
+    textAlign: 'center',
+  },
+
+})
 
 export default HomeScreen;
