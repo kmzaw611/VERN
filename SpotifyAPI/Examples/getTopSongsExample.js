bf2e73997a5ad2563e13a3eedc478a5aa522b314
mwithero <33492855+mwithero@users.noreply.github.com>
2021-02-27 19:09:43 -0500

    Just a test to hopefully figure out if git is working properly

--- SpotifyAPI/Examples/getTopSongsExample.js
+++ SpotifyAPI/Examples/getTopSongsExample.js
@@ -4,10 +4,10 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret: '9ffb3fe2081b414e8c520d19805cbf09', //App client secret
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
-spotifyApi.setRefreshToken('AQAUMHQ_3Gpo7SP_FugQXzPxT3Fg7VHormae_geNuXpGyKvfTcdGFs_AJ1O2ctrxOSXhNRKmmsuHW2giAh656RpRdXsGVSBzUzodatyfPhAbHnl6nYDYlWwLxbpnnLM9lns') // Set refresh token, use auth_check.js to get a refresh token
-//spotifyApi.setAccessToken('BQDWFIQ1gDKfVDY8FU0jvM4tl96E7G3RyFzllznxgoKCJrNwiUWXEsaNAufcwoVtlCuwDq2QJ7D1N2ug5z6GfIO0v0j_kn3dXX4jKrP4NpeYQ38Wl0jhfD1tIXVfSZIrijq2pg-E06z5634uHPG-6Rxs1A')
+spotifyApi.setRefreshToken('AQAzdLg-OI2RDfvhWbnrwyMabkuxAsgQST00VLM9hchMvtwOhKQSg2608fIMOkX_40h-WsXa1i7AVMMw5idxJoyfwFNbOb1bIZHvbZmhedbuF-SAgNNOFdE4zQJOUgxLc24') // Set refresh token, use auth_check.js to get a refresh token
+spotifyApi.setAccessToken('BQCtQpcmjMv6tORrLCr8OtOPupQ8SFf5iCnX01bLquU-B97aJEpgJhVwbKsY33aQUguiIu7vdxF-ctUndkUb9LACLdkrdYc59B3hjrmL_FVL8IgMxftHIwpbw6z9sLuGoEI3cn_mVmMf27TFEt9dnoHcAw')
 //Refresh your access token
-spotifyApi.refreshAccessToken().then(
+/*spotifyApi.refreshAccessToken().then(
     function(data) {
         console.log('Access token refreshed')
         console.log(data.body['access_token'])
@@ -16,7 +16,7 @@ spotifyApi.refreshAccessToken().then(
     function(err) {
         console.log("Error while trying to refresh access token", err);
     }
-);
+);*/
 spotifyApi.getMyTopTracks().then(
     function(data) {
         let topTracks = data.body.items;
