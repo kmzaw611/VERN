5f988bdafd98722c0e7ca01f70af1994021a3896
mwithero <33492855+mwithero@users.noreply.github.com>
2021-02-27 21:03:38 -0500

    Modified getTopSongsExample.js to print out artists
    Created an object that gives us the name  of an artist as well as other data to reference for this information.

--- SpotifyAPI/Examples/getTopSongsExample.js
+++ SpotifyAPI/Examples/getTopSongsExample.js
@@ -4,8 +4,13 @@ var spotifyApi = new SpotifyWebApi({
     clientSecret: '9ffb3fe2081b414e8c520d19805cbf09', //App client secret
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
-spotifyApi.setRefreshToken('AQAzdLg-OI2RDfvhWbnrwyMabkuxAsgQST00VLM9hchMvtwOhKQSg2608fIMOkX_40h-WsXa1i7AVMMw5idxJoyfwFNbOb1bIZHvbZmhedbuF-SAgNNOFdE4zQJOUgxLc24') // Set refresh token, use auth_check.js to get a refresh token
-spotifyApi.setAccessToken('BQCtQpcmjMv6tORrLCr8OtOPupQ8SFf5iCnX01bLquU-B97aJEpgJhVwbKsY33aQUguiIu7vdxF-ctUndkUb9LACLdkrdYc59B3hjrmL_FVL8IgMxftHIwpbw6z9sLuGoEI3cn_mVmMf27TFEt9dnoHcAw')
+//console.log("before error happens 1");
+spotifyApi.setRefreshToken('AQAcpyXOaVpD7UxLo1xYxiflyQVqJ5agLUdFXpTy7SU5X66F-vqvCl6WLNsmbAFd5MQR-cT5xf3AiUI55ZRHgMi4U7NjPsNp-4DGUguG5NuNtuFreqwXu8DnkARp9mWXuzY') // Set refresh token, use auth_check.js to get a refresh token
+//console.log("before error happens 2");
+spotifyApi.setAccessToken('BQBFwr_B5uTqT6UlhDL2EN3OECisL5fp0l_Y9ceqGGm5pcVpnZDhWd7Oj80Wozh3foz9_vvnRooiUJBLMH7ObYqxj4S8503xgocHbnaxZgQY1CTiaxFb-vSP7g3cLmF_6vPRIRtjABji-qA0l6nJd-3I7A')
+
+//console.log("after error happ")
+
 //Refresh your access token
 /*spotifyApi.refreshAccessToken().then(
     function(data) {
@@ -18,9 +23,26 @@ spotifyApi.setAccessToken('BQCtQpcmjMv6tORrLCr8OtOPupQ8SFf5iCnX01bLquU-B97aJEpgJ
     }
 );*/
 spotifyApi.getMyTopTracks().then(
-    function(data) {
-        let topTracks = data.body.items;
-        console.log("The top tracks for this user are: ", topTracks);
+    function (data) {
+        //let topTracks = data.body.items;
+        /*Prints out
+         *  [ { external_urls: [Object],
+            href: 'https://api.spotify.com/v1/artists/41klVmDluQZmGGqoidNfbe',
+            id: '41klVmDluQZmGGqoidNfbe',
+            name: 'Blac Youngsta',
+            type: 'artist',
+            uri: 'spotify:artist:41klVmDluQZmGGqoidNfbe' },
+         * 
+         *from the topT[]
+         **/
+        let topT = [];
+        for (let topTrack of data.body.items) {
+            const track = topTrack.artists
+            topT.push(track);
+            console.log(topT)
+        }
+       // console.log("The top tracks for this user are: ", topTracks);
+        //console.log(Object.keys(data.body.items));
     },
     function(err) {
         console.log("Error while trying to get user's top tracks", err)
