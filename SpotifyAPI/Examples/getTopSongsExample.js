6c6f97e581c23b406af1d7b3076983151da148dc
Carson Rupp <carsonrupp@gmail.com>
2021-02-28 13:21:45 -0500

    Added functionality for parsing the json file returned by Spotify.
    Parsed to obtain a user's top track names and their Spotify ID. Refresh access token still broken

--- SpotifyAPI/Examples/getTopSongsExample.js
+++ SpotifyAPI/Examples/getTopSongsExample.js
@@ -5,13 +5,14 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
 spotifyApi.setRefreshToken('AQAzdLg-OI2RDfvhWbnrwyMabkuxAsgQST00VLM9hchMvtwOhKQSg2608fIMOkX_40h-WsXa1i7AVMMw5idxJoyfwFNbOb1bIZHvbZmhedbuF-SAgNNOFdE4zQJOUgxLc24') // Set refresh token, use auth_check.js to get a refresh token
-spotifyApi.setAccessToken('BQCtQpcmjMv6tORrLCr8OtOPupQ8SFf5iCnX01bLquU-B97aJEpgJhVwbKsY33aQUguiIu7vdxF-ctUndkUb9LACLdkrdYc59B3hjrmL_FVL8IgMxftHIwpbw6z9sLuGoEI3cn_mVmMf27TFEt9dnoHcAw')
+spotifyApi.setAccessToken('BQCoXwsLCUiYwrOAIvx02V8Sveu_nqF-huOYeGOjTcEH1Uu5WXU-FTOV298mmw_AtuHD0Yb7soFuqZ7H_wev1ObjcWyDtKabJKbleV5NhunemVswtZTXUNV0-ONovO_Bb8EDGaIdmIJpylHKdZ_4mpT8oBGX8ei37-sMqGjN-hLrSmnqMexvY5hxv3rq1BUDQAsr9GzyECk2XCv2zbWOBkfaX2Nc3Xly1TWSvxpH_yKq97abqyZmP6_D6Kk9hT_nnK0VDmbVAxa1DolmSY414GvswGU4mwsJ')
 //Refresh your access token
 /*spotifyApi.refreshAccessToken().then(
     function(data) {
         console.log('Access token refreshed')
-        console.log(data.body['access_token'])
-        spotifyApi.setAccessToken(data.body['access_token'])
+        console.log(spotifyApi.getAccessToken())
+        //console.log(data.body['access_token'])
+        //spotifyApi.setAccessToken(data.body['access_token'])
     },
     function(err) {
         console.log("Error while trying to refresh access token", err);
@@ -20,7 +21,17 @@ spotifyApi.setAccessToken('BQCtQpcmjMv6tORrLCr8OtOPupQ8SFf5iCnX01bLquU-B97aJEpgJ
 spotifyApi.getMyTopTracks().then(
     function(data) {
         let topTracks = data.body.items;
-        console.log("The top tracks for this user are: ", topTracks);
+        var songs = [];
+        var i;
+        for (i = 0; i < topTracks.length; i++) {
+            songs.push(topTracks[i].name + " " + topTracks[i].id)
+        }
+        i = 0;
+        for (i = 0; i < songs.length; i++) {
+            console.log(songs[i])
+        }
+        //console.log("The top tracks for this user are: ", topTracks);
+        
     },
     function(err) {
         console.log("Error while trying to get user's top tracks", err)
