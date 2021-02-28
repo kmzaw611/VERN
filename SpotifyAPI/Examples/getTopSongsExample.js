bbc61138cc5c510dcfdedc7ec8911e04e87005fa
mwithero <33492855+mwithero@users.noreply.github.com>
2021-02-27 22:21:16 -0500

    Was able to grab the top songs for a user
    Able to print out the top songs for a user. Still working on actually getting the artists name itself. Able to grab all the information that is inside of the TrackObject that spotify returns to us when we request it. Only issue still being worked on is being able to also grab the artists name too.

--- SpotifyAPI/Examples/getTopSongsExample.js
+++ SpotifyAPI/Examples/getTopSongsExample.js
@@ -1,3 +1,4 @@
+//const { ART } = require('react-native');
 var SpotifyWebApi = require('spotify-web-api-node')
 var spotifyApi = new SpotifyWebApi({
     clientId: '0e8700b7f71d486bbb7c3bd120e892f8', // App client ID
@@ -5,9 +6,9 @@ var spotifyApi = new SpotifyWebApi({
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
 //console.log("before error happens 1");
-spotifyApi.setRefreshToken('AQAcpyXOaVpD7UxLo1xYxiflyQVqJ5agLUdFXpTy7SU5X66F-vqvCl6WLNsmbAFd5MQR-cT5xf3AiUI55ZRHgMi4U7NjPsNp-4DGUguG5NuNtuFreqwXu8DnkARp9mWXuzY') // Set refresh token, use auth_check.js to get a refresh token
+spotifyApi.setRefreshToken('AQCR9BNnDBgq-KHpwU6ZT8xY4UTFCW9vDmThGtQY60lnKU-y1YUBWH5NkQKWSf0tJswZQ2qK2nYdQJuAS47fzeH-8U26Udt8VifqslBK98DpgTh6niew7hS1EEpxpDtl21o') // Set refresh token, use auth_check.js to get a refresh token
 //console.log("before error happens 2");
-spotifyApi.setAccessToken('BQBFwr_B5uTqT6UlhDL2EN3OECisL5fp0l_Y9ceqGGm5pcVpnZDhWd7Oj80Wozh3foz9_vvnRooiUJBLMH7ObYqxj4S8503xgocHbnaxZgQY1CTiaxFb-vSP7g3cLmF_6vPRIRtjABji-qA0l6nJd-3I7A')
+spotifyApi.setAccessToken('BQAaE7GIhcz8R5k5elJQz-nvKuwdlK04RMzr8sfgg9FMlNkyfd_oj2ey1v4xX-nGZtyZSu2CL9FAmwOSGg_ywXT9hGNNLkI2VKeR6I2IhQixAiM8Ud6i-Unf3FpN1g8mJtac3jKlWFoI1kmKdWff34I-vw')
 
 //console.log("after error happ")
 
@@ -22,8 +23,10 @@ spotifyApi.setAccessToken('BQBFwr_B5uTqT6UlhDL2EN3OECisL5fp0l_Y9ceqGGm5pcVpnZDhW
         console.log("Error while trying to refresh access token", err);
     }
 );*/
+
 spotifyApi.getMyTopTracks().then(
     function (data) {
+        let artist_id = [];
         //let topTracks = data.body.items;
         /*Prints out
          *  [ { external_urls: [Object],
@@ -37,9 +40,18 @@ spotifyApi.getMyTopTracks().then(
          **/
         let topT = [];
         for (let topTrack of data.body.items) {
-            const track = topTrack.artists
-            topT.push(track);
-            console.log(topT)
+            //const track = topTrack.artists
+            //1topT.push(track);
+            artist_id.push(topTrack.id);
+            spotifyApi.getArtist(artist_id.pop()).then(function (data) {
+                console.log("Album info: ", data.body);
+            }, function (err) {
+                console.error(err);
+            });
+            //console.log(artist_id)
+            console.log("My top songs are: ", topTrack.name)
+            //console.log(spotifyApi.getArtist(topTrack.id))
+            //console.log(topTrack.data.body.)
         }
        // console.log("The top tracks for this user are: ", topTracks);
         //console.log(Object.keys(data.body.items));
