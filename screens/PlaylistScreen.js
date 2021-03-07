40417c86f5d7b2ae30f05a80620f268724b5eb7a
KyleKMZ <zaw@purdue.edu>
2021-03-07 19:23:34 +0630

    Playlist Screen and test JSON data for playlists

---
+++ screens/PlaylistScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+const PlaylistScreen = ({ navigation }) => {
+  return (
+    <Text>PLAYLIST SCREEN</Text>
+  )
+}
+
+export default PlaylistScreen;
