985cde516b2a3ce1d1df4dc3629308d6457dbf0b
mwithero <33492855+mwithero@users.noreply.github.com>
2021-03-12 01:54:48 -0500

    Added a Follow and Unfollow button option for users to select in the profile screen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,21 +1,31 @@
 import React from 'react'
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native'
+import { Text, View, TouchableOpacity, StyleSheet, Button } from 'react-native'
 
 const ProfileScreen = ({ navigation }) => {
   const onLogoutPress = () => navigation.navigate("StartScreen");
 
-  return (
-    <View>
-      <Text>PROFILE SCREEN</Text>
-      <TouchableOpacity
-        style={styles.logoutButton}
-        onPress={onLogoutPress}
-      >
-        <Text style={styles.logoutText}>Logout</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+    
+        return (
+                <><View>
+                <Text>PROFILE SCREEN</Text>
+                <TouchableOpacity
+                    style={styles.logoutButton}
+                    onPress={onLogoutPress}
+                >
+                    <Text style={styles.logoutText}>Logout</Text>
+                </TouchableOpacity>
+            </View>
+                <View style={styles.followButton}>
+                    <View style={styles.followButtonContainer}>
+                        <Button title="Follow" />
+                    </View>
+                    <View style={styles.followButtonContainer}>
+                        <Button title="Unfollow" />
+                    </View>
+                </View></>
+        )
+    }
+
 
 const styles = StyleSheet.create({
   logoutButton: {
@@ -32,6 +42,15 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: 'white',
   },
+  followButton: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    followButtonContainer: {
+        flex: 1,
+    }
 })
 
 export default ProfileScreen
