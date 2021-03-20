a8a3b1a89cb83551b3322b10d45f7db970fbc4de
KyleKMZ <zaw@purdue.edu>
2021-03-20 13:12:30 +0630

    Created Mongoose model for representing a user's logged in session

---
+++ MondgoDB/models/user_session.js
@@ -0,0 +1,34 @@
+/*
+ * Reference: https://keithweaverca.medium.com/building-a-log-in-system-for-a-mern-stack-39411e9513bd
+ *
+ * This may seem like a redundant model because user.js already exists but it's not.
+ * The user_session model keeps track of whether a user is logged in and whether to start
+ * the app at the login page or the user's home page. It uses the MongoDB document ID
+ * of a user as the identifying token. Something like this is necessary for login/logout
+ * buttons to have actual functionality and state represented in the back-end.
+ *
+ * To clarify, a user_session represents a user's particular session while he is logged
+ * in and using the app, while a user can refer to anyone who has registered for a VERN account.
+ */
+
+const mongoose = require('mongoose');
+
+const userSessionSchema = new mongoose.Schema({
+  // userId is a user's MongoDB object id, although it can be any uniquely identifying value such as email
+  userId: {
+    type: String,
+    default: ''
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now()
+  },
+  // Used to determine whether a user is logged in or not
+  isDeleted: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const UserSession = mongoose.model('UserSession', userSessionSchema);
+module.exports = UserSession;
