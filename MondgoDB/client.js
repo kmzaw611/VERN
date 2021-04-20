1e36ce60d1006d9516e5fa95a9b686476b98da7b
Carson Rupp <carsonrupp@gmail.com>
2021-04-20 16:18:39 -0400

    Changed client to test top songs endpoints

--- MondgoDB/client.js
+++ MondgoDB/client.js
@@ -6,29 +6,30 @@
 const methods = require('./testClient');
 //const prompt = require('prompt');
 
-// data1 = {
+data1 = {
+     refreshToken: "AQCzVfJhvvDF8KvxRKP6ANQ_hfmYbqDdsVQ7XR0FlLlA4soBz6CnmK1HUIas6c9hQHpQ2sV_dDkfYApzJlVkoFMeyjsnVj7Kgdzzd4jG8OwUAbKvzPiVaM5ijhxRhkrdeLU",
+     timeRange: "short",
+     name: "test2"
+ }
+
+methods.publish_top_songs(function (result) {
+console.log(result);
+}, data1);
+
+// data2 = {
 //     refreshToken: "AQCzVfJhvvDF8KvxRKP6ANQ_hfmYbqDdsVQ7XR0FlLlA4soBz6CnmK1HUIas6c9hQHpQ2sV_dDkfYApzJlVkoFMeyjsnVj7Kgdzzd4jG8OwUAbKvzPiVaM5ijhxRhkrdeLU",
-//     timeRange: "short"
+//     timeRange: "short",
+//     description: "endpoint test",
+// }
+// data1 = {
+//     threadID: "6062b12f43a0ad02805f72ae",
+//     username: "Man1",
+//     time: "9:34",
+//     title: "Dayumm",
+//     content: "Content test"
+//     //threadTitle: "Talkin bout sumn"
 // }
 
-// methods.top_songs(function (result) {
+// methods.make_post(function (result) {
 //     console.log(result);
 // }, data1);
-
-data2 = {
-    refreshToken: "AQCzVfJhvvDF8KvxRKP6ANQ_hfmYbqDdsVQ7XR0FlLlA4soBz6CnmK1HUIas6c9hQHpQ2sV_dDkfYApzJlVkoFMeyjsnVj7Kgdzzd4jG8OwUAbKvzPiVaM5ijhxRhkrdeLU",
-    timeRange: "short",
-    description: "endpoint test",
-}
-data1 = {
-    threadID: "6062b12f43a0ad02805f72ae",
-    username: "Man1",
-    time: "9:34",
-    title: "Dayumm",
-    content: "Content test"
-    //threadTitle: "Talkin bout sumn"
-}
-
-methods.make_post(function (result) {
-    console.log(result);
-}, data1);
