4b3bae561c324f938cfa944e11fb8c1ff4ba3e12
Carson Rupp <31084888+ccrupp@users.noreply.github.com>
2021-05-29 16:37:50 -0400

    Update authHandler.js

--- SpotifyAPI/authHandler.js
+++ SpotifyAPI/authHandler.js
@@ -3,8 +3,8 @@ var SpotifyWebApi = require('spotify-web-api-node');
 class AuthenticationHandler {
     constructor() {
         this.spotifyApi = new SpotifyWebApi({
-            clientId: '0e8700b7f71d486bbb7c3bd120e892f8', // App client ID
-            clientSecret: '9ffb3fe2081b414e8c520d19805cbf09', //App client secret
+            clientId: '', // App client ID
+            clientSecret: '', //App client secret
             redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
         });
     }
