e94a47b42b539dc17d9cad351f44d1238a8146ff
Carson Rupp <31084888+ccrupp@users.noreply.github.com>
2021-05-29 16:41:48 -0400

    Update getTopSongsExample.js

--- SpotifyAPI/Examples/getTopSongsExample.js
+++ SpotifyAPI/Examples/getTopSongsExample.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 var SpotifyWebApi = require('spotify-web-api-node')
 var spotifyApi = new SpotifyWebApi({
-    clientId: '0e8700b7f71d486bbb7c3bd120e892f8', // App client ID
-    clientSecret: '9ffb3fe2081b414e8c520d19805cbf09', //App client secret
+    clientId: '', // App client ID
+    clientSecret: '', //App client secret
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
-spotifyApi.setRefreshToken('AQCzVfJhvvDF8KvxRKP6ANQ_hfmYbqDdsVQ7XR0FlLlA4soBz6CnmK1HUIas6c9hQHpQ2sV_dDkfYApzJlVkoFMeyjsnVj7Kgdzzd4jG8OwUAbKvzPiVaM5ijhxRhkrdeLU') // Set refresh token, use auth_check.js to get a refresh token
-spotifyApi.setAccessToken('BQAaE7GIhcz8R5k5elJQz-nvKuwdlK04RMzr8sfgg9FMlNkyfd_oj2ey1v4xX-nGZtyZSu2CL9FAmwOSGg_ywXT9hGNNLkI2VKeR6I2IhQixAiM8Ud6i-Unf3FpN1g8mJtac3jKlWFoI1kmKdWff34I-vw')
+spotifyApi.setRefreshToken('') // Set refresh token, use auth_check.js to get a refresh token
+spotifyApi.setAccessToken('')
 var userId = ""
 
 spotifyApi.refreshAccessToken()
