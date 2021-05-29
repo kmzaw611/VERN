0d3f6cb089255f5fdb3c0c3b2d69a4bfd903c32d
Carson Rupp <31084888+ccrupp@users.noreply.github.com>
2021-05-29 16:39:57 -0400

    Update getPlayListExample.js

--- SpotifyAPI/Examples/getPlayListExample.js
+++ SpotifyAPI/Examples/getPlayListExample.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 var SpotifyWebApi = require('spotify-web-api-node')
 var spotifyApi = new SpotifyWebApi({
-    clientId: '0e8700b7f71d486bbb7c3bd120e892f8', // App client ID
-    clientSecret: '9ffb3fe2081b414e8c520d19805cbf09', //App client secret
+    clientId: '', // App client ID
+    clientSecret: '', //App client secret
     redirectUri: 'http://localhost:8888/callback' //Where the user is to be taken after authentication
 });
 spotifyApi.setRefreshToken('AQCzVfJhvvDF8KvxRKP6ANQ_hfmYbqDdsVQ7XR0FlLlA4soBz6CnmK1HUIas6c9hQHpQ2sV_dDkfYApzJlVkoFMeyjsnVj7Kgdzzd4jG8OwUAbKvzPiVaM5ijhxRhkrdeLU') // Set refresh token, use auth_check.js to get a refresh token
